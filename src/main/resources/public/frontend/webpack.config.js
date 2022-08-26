fbc3dc79b05b18c8f31b407e9aa9b6ca966ea93f
Sylvain Tran <45033804+SylvainTran@users.noreply.github.com>
2022-08-26 16:00:30 -0400

    Fixed assets workspace config problem
    - Images and resources load normally (tested locally)

--- src/main/resources/public/frontend/webpack.config.js
+++ src/main/resources/public/frontend/webpack.config.js
@@ -7,7 +7,7 @@ const isProduction = process.env.NODE_ENV == "production";
 const stylesHandler = "style-loader";
 
 const config = {
-  entry: "./src/index.html",
+  entry: "./index.html",
   output: {
     path: path.resolve(__dirname, "dist"),
   },
